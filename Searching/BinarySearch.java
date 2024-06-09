15d46060864ab87937b1212cd96629c25eaa9db3
punitkrrishn <punitkrrishn9@gmail.com>
2024-06-09 22:07:53 +0530

    Binary Search to find X in sorted array

--- Searching/BinarySearch.java
+++ Searching/BinarySearch.java
@@ -6,7 +6,8 @@ class BinarySearch {
         
         int[] a = {3, 4, 6, 7, 9, 12, 16, 17};
         int target = 6;
-        int ind = binarySearch(a, target);
+        //int ind = binarySearch(a, target);
+        int ind = search(a, target);
         if (ind == -1)
             System.out.println("The target is not present.");
         else
@@ -27,5 +28,23 @@ class BinarySearch {
         }
         return -1;
     }
+
+    //-------------------------Optimal Approach--------------------------
+
+    public static int binarySearchOptimal(int[] nums, int low, int high, int target) {
+        if (low > high) return -1; //Base case.
+
+        // Perform the steps:
+        int mid = (low + high) / 2;
+        if (nums[mid] == target) return mid;
+        else if (target > nums[mid])
+            return binarySearchOptimal(nums, mid + 1, high, target);
+        return binarySearchOptimal(nums, low, mid - 1, target);
+    }
+
+    public static int search(int[] nums, int target) {
+        return binarySearchOptimal(nums, 0, nums.length - 1, target);
+    }
+
     
 }
